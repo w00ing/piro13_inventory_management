6c1ecb2568041da82f95e55bc0fbcb7a4e2f8ac5
w00ing <kimwy1997@gmail.com>
2020-07-30 00:58:05 +0900

    Create Customers model

--- inventory_management/customers/models.py
+++ inventory_management/customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from phone_field import PhoneField
 
-# Create your models here.
+
+class Customer(models.Model):
+
+    """ Customer Model Definition """
+
+    name = models.CharField(max_length=25)
+    phone_number = PhoneField()
+    address = models.CharField(max_length=140)
+
